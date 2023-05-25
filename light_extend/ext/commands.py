87fc2807c676bd3e1cb06465de5d97a4b77b4bbc
blessedtoaster12 <blessedtoaster12@users.noreply.github.com>
2023-05-25 16:30:57 +0000

    ✅ Ready to clone and code.

--- light_extend/ext/commands.py
+++ light_extend/ext/commands.py
@@ -1,8 +1,8 @@
 import click
 
-from project_name.ext.auth import create_user
-from project_name.ext.database import db
-from project_name.models import Product
+from light_extend.ext.auth import create_user
+from light_extend.ext.database import db
+from light_extend.models import Product
 
 
 def create_db():
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-"""Python setup.py for project_name package"""
+"""Python setup.py for light_extend package"""
 import io
 import os
 from setuptools import find_packages, setup
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 def read(*paths, **kwargs):
     """Read the contents of a text file safely.
-    >>> read("project_name", "VERSION")
+    >>> read("light_extend", "VERSION")
     '0.1.0'
     >>> read("README.md")
     ...
@@ -30,17 +30,17 @@ def read_requirements(path):
 
 
 setup(
-    name="project_name",
-    version=read("project_name", "VERSION"),
-    description="project_description",
-    url="https://github.com/author_name/project_urlname/",
+    name="light_extend",
+    version=read("light_extend", "VERSION"),
+    description="Awesome light_extend created by blessedtoaster12",
+    url="https://github.com/blessedtoaster12/light-extend/",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
-    author="author_name",
+    author="blessedtoaster12",
     packages=find_packages(exclude=["tests", ".github"]),
     install_requires=read_requirements("requirements.txt"),
     entry_points={
-        "console_scripts": ["project_name = project_name.__main__:main"]
+        "console_scripts": ["light_extend = light_extend.__main__:main"]
     },
     extras_require={
         "test": read_requirements("requirements-test.txt")
